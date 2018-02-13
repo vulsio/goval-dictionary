85c10368a38d0c020d76b8bd93155f00324dcb08
Kota Kanbe <kotakanbe@gmail.com>
2018-02-13 10:41:18 +0900

    fix: change gorm column size of Definition>Description (#30)

--- models/models.go
+++ models/models.go
@@ -29,7 +29,7 @@ type Definition struct {
 
 	DefinitionID  string
 	Title         string
-	Description   string
+	Description   string `gorm:"size:4096"`
 	Advisory      Advisory
 	Debian        Debian
 	AffectedPacks []Package

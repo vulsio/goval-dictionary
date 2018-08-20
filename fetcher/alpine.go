cd72224572fff64ee8fb8bdb6256a675be5c7c89
kota kanbe <kotakanbe@gmail.com>
2018-08-20 20:34:57 +0900

    fix(fetch): fix 403 while fetching redhat oval
    https://github.com/future-architect/vuls/issues/698

--- fetcher/alpine.go
+++ fetcher/alpine.go
@@ -28,7 +28,7 @@ func FetchAlpineFiles(versions []string) ([]FetchResult, error) {
 		return nil,
 			fmt.Errorf("There are no versions to fetch")
 	}
-	results, err := fetchFeedFileConcurrently(reqs)
+	results, err := fetchFeedFiles(reqs)
 	if err != nil {
 		return nil,
 			fmt.Errorf("Failed to fetch. err: %s", err)

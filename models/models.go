2d409e696461a095d5c969487c6b7a57d15711a4
kota kanbe <kotakanbe@gmail.com>
2018-11-27 19:56:53 +0900

    fix(rdb): col type varchar to text

--- models/models.go
+++ models/models.go
@@ -29,7 +29,7 @@ type Definition struct {
 
 	DefinitionID  string
 	Title         string
-	Description   string `gorm:"size:8192"`
+	Description   string `sql:"type:text"`
 	Advisory      Advisory
 	Debian        Debian
 	AffectedPacks []Package

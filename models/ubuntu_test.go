2bc09b1fd6bcf00ec724f261a3feb43c2194a37f
Kota Kanbe <kotakanbe@gmail.com>
2019-05-20 11:20:33 +0900

    fix(ubuntu): Support latest OVAL format for Ubuntu (#60)

---
+++ models/ubuntu_test.go
@@ -0,0 +1,92 @@
+package models
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/k0kubun/pp"
+)
+
+func TestParseNotFixedYet(t *testing.T) {
+	var tests = []struct {
+		comment  string
+		expected Package
+	}{
+		{
+			comment: `xine-console package in bionic is affected and needs fixing.`,
+			expected: Package{
+				Name:        "xine-console",
+				NotFixedYet: true,
+			},
+		},
+	}
+
+	for i, tt := range tests {
+		actual, ok := parseNotFixedYet(tt.comment)
+		if !ok {
+			t.Errorf("[%d]: no match: %s\n", i, tt.comment)
+			return
+		}
+		if !reflect.DeepEqual(tt.expected, *actual) {
+			e := pp.Sprintf("%v", tt.expected)
+			a := pp.Sprintf("%v", *actual)
+			t.Errorf("[%d]: expected: %s\n, actual: %s\n", i, e, a)
+		}
+	}
+}
+
+func TestParseNotDecided(t *testing.T) {
+	var tests = []struct {
+		comment  string
+		expected Package
+	}{
+		{
+			comment: `libxerces-c-samples package in bionic is affected, but a decision has been made to defer addressing it (note: '2019-01-01').`,
+			expected: Package{
+				Name:        "libxerces-c-samples",
+				NotFixedYet: true,
+			},
+		},
+	}
+
+	for i, tt := range tests {
+		actual, ok := parseNotDecided(tt.comment)
+		if !ok {
+			t.Errorf("[%d]: no match: %s\n", i, tt.comment)
+			return
+		}
+		if !reflect.DeepEqual(tt.expected, *actual) {
+			e := pp.Sprintf("%v", tt.expected)
+			a := pp.Sprintf("%v", *actual)
+			t.Errorf("[%d]: expected: %s\n, actual: %s\n", i, e, a)
+		}
+	}
+}
+
+func TestParseFixed(t *testing.T) {
+	var tests = []struct {
+		comment  string
+		expected Package
+	}{
+		{
+			comment: `iproute2 package in bionic, is related to the CVE in some way and has been fixed (note: '3.12.0-2').`,
+			expected: Package{
+				Name:    "iproute2",
+				Version: "3.12.0-2",
+			},
+		},
+	}
+
+	for i, tt := range tests {
+		actual, ok := parseFixed(tt.comment)
+		if !ok {
+			t.Errorf("[%d]: no match: %s\n", i, tt.comment)
+			return
+		}
+		if !reflect.DeepEqual(tt.expected, *actual) {
+			e := pp.Sprintf("%v", tt.expected)
+			a := pp.Sprintf("%v", *actual)
+			t.Errorf("[%d]: expected: %s\n, actual: %s\n", i, e, a)
+		}
+	}
+}

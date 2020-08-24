7cff9b59c35530ba3925c2d24cfb21194f661db1
MaineK00n <dev.pylori1229@gmail.com>
2020-08-25 05:06:42 +0900

    alpine support -by-cveid option

--- db/rdb/alpine.go
+++ db/rdb/alpine.go
@@ -105,5 +105,18 @@ func (o *Alpine) GetByPackName(driver *gorm.DB, osVer, packName, _ string) (defs
 
 // GetByCveID select definition by CveID
 func (o *Alpine) GetByCveID(driver *gorm.DB, osVer, cveID string) (defs []models.Definition, err error) {
+	err = driver.Joins("JOIN roots ON roots.id = definitions.root_id AND roots.family= ? AND roots.os_version = ?",
+		config.Alpine, majorDotMinor(osVer)).
+		Joins("JOIN advisories ON advisories.definition_id = definitions.id").
+		Joins("JOIN cves ON cves.advisory_id = advisories.id").
+		Where("cves.cve_id = ?", cveID).
+		Preload("Advisory").
+		Preload("Advisory.Cves").
+		Preload("AffectedPacks").
+		Preload("References").
+		Find(&defs).Error
+	if err != nil && err != gorm.ErrRecordNotFound {
+		return nil, err
+	}
 	return defs, nil
 }

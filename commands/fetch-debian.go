3ca681d190ca16de4454e9e84c7ff2a320b4f3e2
MaineK00n <mainek00n.1229@gmail.com>
2025-04-09 18:29:16 +0900

    chore: fix lint error

--- commands/fetch-debian.go
+++ commands/fetch-debian.go
@@ -3,6 +3,7 @@ package commands
 import (
 	"bytes"
 	"encoding/xml"
+	"errors"
 	"strings"
 	"time"
 
@@ -42,7 +43,7 @@ func fetchDebian(_ *cobra.Command, args []string) (err error) {
 
 	driver, err := db.NewDB(viper.GetString("dbtype"), viper.GetString("dbpath"), viper.GetBool("debug-sql"), db.Option{})
 	if err != nil {
-		if xerrors.Is(err, db.ErrDBLocked) {
+		if errors.Is(err, db.ErrDBLocked) {
 			return xerrors.Errorf("Failed to open DB. Close DB connection before fetching. err: %w", err)
 		}
 		return xerrors.Errorf("Failed to open DB. err: %w", err)
